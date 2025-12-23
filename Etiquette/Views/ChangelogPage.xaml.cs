22199821df41ed93ba2c2814a547a135e66fb4dc
DEHER Rémi <remi2r@hotmail.fr>
2025-12-23 17:36:46 +0100

    Update version in manifest

--- Etiquette/Views/ChangelogPage.xaml.cs
+++ Etiquette/Views/ChangelogPage.xaml.cs
@@ -4,12 +4,10 @@ using Microsoft.UI.Xaml;
 
 namespace Etiquette.Views
 {
-    // Le nom ici doit être "ChangelogPage", PAS "AboutPage"
     public sealed partial class ChangelogPage : Page
     {
         public ChangelogPage()
         {
-            // Cette méthode n'existe que si x:Class dans le XAML correspond à ce nom de classe
             this.InitializeComponent();
         }
     }
